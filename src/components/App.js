7970957dadd83378092ffb0008f9fc0a70a90a32
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-10-20 21:51:18 +0100

    setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styling/app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Secret Santa App</h1>
+    </div>
+  );
+}
+
+export default App;
