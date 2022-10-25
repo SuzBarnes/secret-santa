2ecfee983a68a750655e4d70b51c4ae699107b3c
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-10-25 21:22:08 +0100

    navbar working test

--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import NavBar from "../components/NavBar";
 import { BrowserRouter } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 describe("NavBar", () => {
   it("renders correctly", () => {
