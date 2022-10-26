1b1a88e19081c695f201f6723850cfadb7028cef
Lisa Heffernan <lisaheffernan@outlook.com>
2022-10-26 11:55:09 +0100

    account details component renders the mock json data, excluding password

--- src/components/AccountDetails.js
+++ src/components/AccountDetails.js
@@ -1,10 +1,36 @@
 import React from "react";
 import "../styles/accountdetails.scss";
+import user from "../data/user.json";
+import likesDislikes from "../data/likesDislikes.json";
 
 const AccountDetails = () => {
   return (
-    <div>
-      <h1>Account Details.</h1>
+    <div className="account-details-container">
+      <div className="account-details-title">
+        Account Details. <button type="submit">edit</button>
+      </div>
+      <div className="fields-container">
+        <div className="field-card">
+          <div className="field-tag">name </div>
+          <div className="field-value">{user.users[0].name}</div>
+        </div>
+        <div className="field-card">
+          <div className="field-tag">email </div>
+          <div className="field-value">{user.users[0].email}</div>
+        </div>
+        <div className="field-card">
+          <div className="field-tag">likes</div>
+          {likesDislikes.likesDislikes.likes.split(", ").map((item) => (
+            <div className="field-value">{item}</div>
+          ))}
+        </div>
+        <div className="field-card">
+          <div className="field-tag">dislikes </div>
+          {likesDislikes.likesDislikes.dislikes.split(", ").map((item) => (
+            <div className="field-value">{item}</div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
