614fde9da51003867985d884d93289b25c2152dc
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-10-26 16:05:54 +0100

    error not switching

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,169 +9,169 @@ import Alert from "./Alert";
 const LOGIN_URL = "/posts";
 
 const Login = () => {
-  // const { setAuth } = useContext(AuthContext);
+    // const { setAuth } = useContext(AuthContext);
 
-  const initialState = {
-    login: {
-      email: "",
-      password: "",
-    },
-    register: {
-      firstname: "",
-      surname: "",
-      email: "",
-      password: "",
-    },
-    alert: {
-      message: "",
-      isSuccess: false,
-    },
-  };
-  const [login, setLogin] = useState(initialState.login);
-  const [register, setRegister] = useState(initialState.register);
-  const [passwordShown, setPasswordShown] = useState(false);
-  const [alert, setAlert] = useState(initialState.alert);
-
-  const togglePassword = () => {
-    setPasswordShown(!passwordShown);
-  };
-
-  const handleLogin = (event) => {
-    event.preventDefault();
-    setAlert({ message: "", isSuccess: false });
-
-    axios
-      .post(LOGIN_URL, JSON.stringify(login), {
-        headers: {
-          "Content-Type": "application/json",
-          withCredentials: true,
+    const initialState = {
+        login: {
+            email: "",
+            password: "",
         },
-      })
-      .then(() => {
-        setAlert({
-          message: `Welcome ${login.email}`,
-          // want it ideally to be welcome register.first name, but not sure how it will access the firstname from the original registration//
-          isSuccess: true,
-        });
-      })
-      .catch(() => {
-        setAlert({
-          message: "Server error, please try again later",
-          isSuccess: false,
-        });
-      });
-  };
-  const handleRegister = (event) => {
-    event.preventDefault();
-    setAlert({ message: "", isSuccess: false });
-    console.log(register);
-    axios
-      .post(LOGIN_URL, JSON.stringify(register), {
-        headers: {
-          "Content-Type": "application/json",
-          withCredentials: true,
+        register: {
+            firstname: "",
+            surname: "",
+            email: "",
+            password: "",
         },
-      })
-      .then(() => {
-        setAlert({
-          message: `Merry Christmas and welcome ${register.firstname}!`,
-          isSuccess: true,
-        });
-      })
-      .catch(() => {
-        setAlert({
-          message: "Server error, please try again later",
-          isSuccess: false,
-        });
-      });
-  };
-  const handleLoginChange = (event) => {
-    setLogin({ ...login, [event.target.name]: event.target.value });
-  };
-  const handleRegisterChange = (event) => {
-    setRegister({ ...register, [event.target.name]: event.target.value });
-  };
+        alert: {
+            message: "",
+            isSuccess: false,
+        },
+    };
+    const [login, setLogin] = useState(initialState.login);
+    const [register, setRegister] = useState(initialState.register);
+    const [passwordShown, setPasswordShown] = useState(false);
+    const [alert, setAlert] = useState(initialState.alert);
 
-  return (
-    <div className="login">
-      <h3>Login</h3>
-      <form onSubmit={handleLogin}>
-        <Alert message={alert.message} success={alert.isSuccess} />
-        <input
-          id="email"
-          name="email"
-          placeholder="Email Address"
-          value={login.email}
-          onChange={handleLoginChange}
-          required
-        />
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown);
+    };
 
-        <input
-          id="password"
-          name="password"
-          type={passwordShown ? "text" : "password"}
-          placeholder="Enter valid password"
-          value={login.password}
-          onChange={handleLoginChange}
-          required
-        />
-        <button type="submit" onClick={togglePassword}>
-          <FontAwesomeIcon icon={faEye} className="font-awesome" />
-        </button>
-        <button type="submit">Log in</button>
-      </form>
-      <p>
-        Need an Account?
-        <span className="line">
-          {/* put router link here */}
-          {/* <a href="login">Sign Up</a> */}
-        </span>
-      </p>
-      <h3>Register</h3>
-      <form onSubmit={handleRegister}>
-        <Alert message={alert.message} success={alert.isSuccess} />
+    const handleLogin = (event) => {
+        event.preventDefault();
+        setAlert({ message: "", isSuccess: false });
 
-        <input
-          id="firstname"
-          name="firstname"
-          placeholder="Enter First Name"
-          value={register.firstname}
-          onChange={handleRegisterChange}
-          required
-        />
-        <input
-          id="surname"
-          name="surname"
-          placeholder="Enter Surname"
-          value={register.surname}
-          onChange={handleRegisterChange}
-          required
-        />
-        <input
-          id="email"
-          name="email"
-          placeholder="Email Address"
-          value={register.email}
-          onChange={handleRegisterChange}
-          required
-        />
+        axios
+            .post(LOGIN_URL, JSON.stringify(login), {
+                headers: {
+                    "Content-Type": "application/json",
+                    withCredentials: true,
+                },
+            })
+            .then(() => {
+                setAlert({
+                    message: `Welcome ${login.email}`,
+                    // want it ideally to be welcome register.first name, but not sure how it will access the firstname from the original registration//
+                    isSuccess: true,
+                });
+            })
+            .catch(() => {
+                setAlert({
+                    message: "Server error, please try again later",
+                    isSuccess: false,
+                });
+            });
+    };
+    const handleRegister = (event) => {
+        event.preventDefault();
+        setAlert({ message: "", isSuccess: false });
+        console.log(register);
+        axios
+            .post(LOGIN_URL, JSON.stringify(register), {
+                headers: {
+                    "Content-Type": "application/json",
+                    withCredentials: true,
+                },
+            })
+            .then(() => {
+                setAlert({
+                    message: `Merry Christmas and welcome ${register.firstname}!`,
+                    isSuccess: true,
+                });
+            })
+            .catch(() => {
+                setAlert({
+                    message: "Server error, please try again later",
+                    isSuccess: false,
+                });
+            });
+    };
+    const handleLoginChange = (event) => {
+        setLogin({ ...login, [event.target.name]: event.target.value });
+    };
+    const handleRegisterChange = (event) => {
+        setRegister({ ...register, [event.target.name]: event.target.value });
+    };
 
-        <input
-          id="password"
-          name="password"
-          type={passwordShown ? "text" : "password"}
-          placeholder="Enter valid password"
-          value={register.password}
-          onChange={handleRegisterChange}
-          required
-        />
-        <button type="submit" onClick={togglePassword}>
-          <FontAwesomeIcon icon={faEye} className="font-awesome" />
-        </button>
+    return (
+        <div className="login">
+            <h3>Login</h3>
+            <form onSubmit={handleLogin}>
+                <Alert message={alert.message} success={alert.isSuccess} />
+                <input
+                    id="email"
+                    name="email"
+                    placeholder="Email Address"
+                    value={login.email}
+                    onChange={handleLoginChange}
+                    required
+                />
 
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  );
+                <input
+                    id="password"
+                    name="password"
+                    type={passwordShown ? "text" : "password"}
+                    placeholder="Enter valid password"
+                    value={login.password}
+                    onChange={handleLoginChange}
+                    required
+                />
+                <button type="submit" onClick={togglePassword}>
+                    <FontAwesomeIcon icon={faEye} className="font-awesome" />
+                </button>
+                <button type="submit">Log in</button>
+            </form>
+            <p>
+                Need an Account?
+                <span className="line">
+                    {/* put router link here */}
+                    {/* <a href="login">Sign Up</a> */}
+                </span>
+            </p>
+            <h3>Register</h3>
+            <form onSubmit={handleRegister}>
+                <Alert message={alert.message} success={alert.isSuccess} />
+
+                <input
+                    id="firstname"
+                    name="firstname"
+                    placeholder="Enter First Name"
+                    value={register.firstname}
+                    onChange={handleRegisterChange}
+                    required
+                />
+                <input
+                    id="surname"
+                    name="surname"
+                    placeholder="Enter Surname"
+                    value={register.surname}
+                    onChange={handleRegisterChange}
+                    required
+                />
+                <input
+                    id="email"
+                    name="email"
+                    placeholder="Email Address"
+                    value={register.email}
+                    onChange={handleRegisterChange}
+                    required
+                />
+
+                <input
+                    id="password"
+                    name="password"
+                    type={passwordShown ? "text" : "password"}
+                    placeholder="Enter valid password"
+                    value={register.password}
+                    onChange={handleRegisterChange}
+                    required
+                />
+                <button type="submit" onClick={togglePassword}>
+                    <FontAwesomeIcon icon={faEye} className="font-awesome" />
+                </button>
+
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
 };
 export default Login;
