07192823eed3fe7718526dfe46e3fa23f6dcf923
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-10-26 15:01:06 +0100

    toggle button for password, need to link axios to backend for it to store information

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,109 +1,154 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
+// import axios from "../api/axios";
 import "../styles/login.scss";
+// import AuthContext from "../context/AuthProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+
+// const LOGIN_URL = "/login";
 
 const Login = () => {
-    const userRef = useRef();
-    const errRef = useRef();
+  // const { setAuth } = useContext(AuthContext);
 
-    const initialState = {
-        login: {
-            email: "",
-            password: "",
-        },
-        register: {
-            firstname: "",
-            surname: "",
-            email: "",
-            password: "",
-        },
-        alert: {
-            message: "",
-            isSuccess: false,
-        },
-    };
-    const [login, setLogin] = useState(initialState.login);
-    const [register, setRegister] = useState(initialState.register);
-    const [alert, setAlert] = useState(initialState.alert);
+  const initialState = {
+    login: {
+      email: "",
+      password: "",
+    },
+    register: {
+      firstname: "",
+      surname: "",
+      email: "",
+      password: "",
+    },
+    alert: {
+      message: "",
+      isSuccess: false,
+    },
+  };
+  const [login, setLogin] = useState(initialState.login);
+  const [register, setRegister] = useState(initialState.register);
+  const [passwordShown, setPasswordShown] = useState(false);
+  const [alert, setAlert] = useState(initialState.alert);
 
-    const handleLogin = (event) => {
-        event.preventDefault();
-        setAlert({ message: "", isSuccess: false });
-        console.log(login);
-    };
-    const handleRegister = (event) => {
-        event.preventDefault();
-        setAlert({ message: "", isSuccess: false });
-        console.log(register);
-    };
-    const handleLoginChange = (event) => {
-        setLogin({ ...login, [event.target.name]: event.target.value });
-    };
-    const handleRegisterChange = (event) => {
-        setRegister({ ...register, [event.target.name]: event.target.value });
-    };
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
 
-    return (
-        <div className="login">
-            <h3>Login</h3>
-            <form onSubmit={handleLogin}>
-                <input
-                    id="login-email"
-                    name="login-email"
-                    placeholder="Email Address"
-                    value={login.email}
-                    onChange={handleLoginChange}
-                    required
-                />
+  const handleLogin = (event) => {
+    event.preventDefault();
+    setAlert({ message: "", isSuccess: false });
 
-                <input
-                    id="login-password"
-                    name="login-password"
-                    placeholder="Enter valid password"
-                    value={login.password}
-                    onChange={handleLoginChange}
-                    required
-                />
-                <button type="submit">Log in</button>
-            </form>
+    // axios
+    //     .post(LOGIN_URL, JSON.stringify(login), {
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             withCredentials: true,
+    //         },
+    //     })
+    //     .then(() => {
+    //         setAlert({
+    //             message: `Welcome ${login.email}`,
+    //             // want it ideally to be welcome register.first name, but not sure how it will access the firstname from the original registration//
+    //             isSuccess: true,
+    //         });
+    //     })
+    //     .catch(() => {
+    //         setAlert({
+    //             message: "Server error, please try again later",
+    //             isSuccess: false,
+    //         });
+    //     });
+  };
+  const handleRegister = (event) => {
+    event.preventDefault();
+    setAlert({ message: "", isSuccess: false });
+    console.log(register);
+  };
+  const handleLoginChange = (event) => {
+    setLogin({ ...login, [event.target.name]: event.target.value });
+  };
+  const handleRegisterChange = (event) => {
+    setRegister({ ...register, [event.target.name]: event.target.value });
+  };
 
-            <h3>Register</h3>
-            <form onSubmit={handleRegister}>
-                <input
-                    id="firstname"
-                    name="firstname"
-                    placeholder="Enter First Name"
-                    value={register.firstname}
-                    onChange={handleRegisterChange}
-                    required
-                />
-                <input
-                    id="surname"
-                    name="surname"
-                    placeholder="Enter Surname"
-                    value={register.surname}
-                    onChange={handleRegisterChange}
-                    required
-                />
-                <input
-                    id="register-email"
-                    name="register-email"
-                    placeholder="Email Address"
-                    value={register.email}
-                    onChange={handleRegisterChange}
-                    required
-                />
+  return (
+    <div className="login">
+      <h3>Login</h3>
+      <form onSubmit={handleLogin}>
+        <input
+          id="email"
+          name="email"
+          placeholder="Email Address"
+          value={login.email}
+          onChange={handleLoginChange}
+          required
+        />
 
-                <input
-                    id="register-password"
-                    name="register-password"
-                    placeholder="Enter valid password"
-                    value={register.password}
-                    onChange={handleRegisterChange}
-                    required
-                />
-                <button type="submit">Register</button>
-            </form>
-        </div>
-    );
+        <input
+          id="password"
+          name="password"
+          type={passwordShown ? "text" : "password"}
+          placeholder="Enter valid password"
+          value={login.password}
+          onChange={handleLoginChange}
+          required
+        />
+        <button type="submit" onClick={togglePassword}>
+          <FontAwesomeIcon icon={faEye} className="font-awesome" />
+        </button>
+        <button type="submit">Log in</button>
+      </form>
+      <p>
+        Need an Account?
+        <span className="line">
+          {/* put router link here */}
+          {/* <a href="login">Sign Up</a> */}
+        </span>
+      </p>
+      <h3>Register</h3>
+      <form onSubmit={handleRegister}>
+        <input
+          id="firstname"
+          name="firstname"
+          placeholder="Enter First Name"
+          value={register.firstname}
+          onChange={handleRegisterChange}
+          required
+        />
+        <input
+          id="surname"
+          name="surname"
+          placeholder="Enter Surname"
+          value={register.surname}
+          onChange={handleRegisterChange}
+          required
+        />
+        <input
+          id="email"
+          name="email"
+          placeholder="Email Address"
+          value={register.email}
+          onChange={handleRegisterChange}
+          required
+        />
+
+        <input
+          id="password"
+          name="password"
+          type={passwordShown ? "text" : "password"}
+          placeholder="Enter valid password"
+          value={register.password}
+          onChange={handleRegisterChange}
+          required
+        />
+        <button type="submit" onClick={togglePassword}>
+          <FontAwesomeIcon icon={faEye} className="font-awesome" />
+        </button>
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
 export default Login;
