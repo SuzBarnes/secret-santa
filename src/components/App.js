c8a3d9565ab2de84eb525e5d9dd44c51f144282d
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-10-26 10:24:20 +0100

    moved login so it renders correctly in navbar for now

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,6 @@ const App = () => {
   return (
     <div className="App">
       <h1>Secret Santa App</h1>
-      <Login />
       <NavBar />
       <Routes>
         <Route path="/" element={<MyEvents />} />
