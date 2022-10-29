404c4992ad0455057de60f2238551c52851f34e0
AlexPBradley <apbradley@hotmail.com>
2022-10-29 20:36:48 +0100

    create event setup

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -3,8 +3,21 @@ import "../styles/createevent.scss";
 
 const CreateEvent = () => {
   return (
-    <div>
-      <h1>Create Event</h1>
+    <div className="create-event">
+      <form className="create-event-form">
+        <input className="create-event-title" type="text" placeholder="Title" />
+        <input
+          className="create-event-price"
+          type="number"
+          placeholder="Price Limit"
+        />
+        <input
+          id="test"
+          className="create-event-exchange-date"
+          type="date"
+          placeholder="Exchange Date"
+        />
+      </form>
     </div>
   );
 };
