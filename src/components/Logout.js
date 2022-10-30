86098bd19b08fdbc53845f4e585c44a7b96fdbbd
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-10-30 21:02:50 +0000

    logout working but not in the best way

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Logout = () => {
+  const initialState = {
+    alert: {
+      message: "",
+      isSuccess: false,
+    },
+  };
+  const [alert, setAlert] = useState(initialState.alert);
+  const logoutClick = () => {
+    localStorage.clear();
+    setAlert({
+      message: "You have successfully logged out",
+      isSuccess: true,
+    });
+  };
+
   return (
     <div className="logout">
-      <button type="submit">Logout</button>
+      <button type="submit" onClick={logoutClick}>
+        Logout
+      </button>
     </div>
   );
 };
--- src/components/NavBarTop.js
+++ src/components/NavBarTop.js
@@ -1,32 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/navbartop.scss";
 import secretSantLogo from "../images/SS-logo.png";
-import Logout from "./Logout";
 
 const NavBarTop = () => {
-  const [loggedIn, setLoggedIn] = useState(!localStorage.access);
-
-  if (loggedIn) {
-    return (
-      <div className="navbartop">
-        <div className="navbar-logo-container">
-          <img
-            className="ss-logo"
-            alt="secret santa logo"
-            src={secretSantLogo}
-          />
-        </div>
-        <ul className="navbartop-links">
-          <li>
-            <Link className="navbartop-link-item" to="/">
-              <Logout />
-            </Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
   return (
     <div className="navbartop">
       <div className="navbar-logo-container">
@@ -41,6 +18,11 @@ const NavBarTop = () => {
         <li>
           <Link className="navbartop-link-item" to="register">
             Register
+          </Link>
+        </li>
+        <li>
+          <Link className="navbartop-link-item" to="logout">
+            Logout
           </Link>
         </li>
       </ul>
