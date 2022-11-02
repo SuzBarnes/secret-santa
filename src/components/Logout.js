6dd9c4e90cf88a39da19ae2be0cb9a67d5035b66
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-11-02 01:49:06 +0000

    working, refreshing, storing data and clearing data, can now refresh and when logout, it works

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -16,6 +16,12 @@ const Logout = () => {
   const { setUserId } = useAuthContext();
   const [alert, setAlert] = useState(initialState.alert);
   const navigate = useNavigate();
+  // to get the component to refresh while redirecting, thus clearing whatever details it had stored you need the changeLocation function in the onClick
+  const changeLocation = (redirect) => {
+    navigate(redirect, { replace: true });
+    window.location.reload();
+  };
+  // clear local and session storage, change the userId to "", display the alert, and then refresh and redirect
   const logoutClick = () => {
     localStorage.clear();
     sessionStorage.clear();
@@ -24,7 +30,7 @@ const Logout = () => {
       message: "You have successfully logged out",
       isSuccess: true,
     });
-    navigate("/");
+    changeLocation("/");
   };
   return (
     <div className="logout">
